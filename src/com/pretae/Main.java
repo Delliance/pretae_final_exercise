a9ec6f44995cef6ef966708162bb22d413a0b6dc
danielgn95 <daniel.gn@globant.com>
2022-04-01 15:32:57 -0500

    advanced_calculator

--- src/com/pretae/Main.java
+++ src/com/pretae/Main.java
@@ -14,4 +14,13 @@ public class Main {
         return number1 - number2;
     }
 
+
+    public double multiply (double number1, double number2){
+        return number1 * number2;
+    }
+
+    public double divide (double number1, double number2){
+        return number1 / number2;
+    }
+
 }
