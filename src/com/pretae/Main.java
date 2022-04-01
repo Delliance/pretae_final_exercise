834bd55b9243de7a240fabdb34f772f470ac30f8
danielgn95 <daniel.gn@globant.com>
2022-04-01 15:32:17 -0500

    base_calculator

--- src/com/pretae/Main.java
+++ src/com/pretae/Main.java
@@ -5,4 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public double add (double number1, double number2){
+        return number1 + number2;
+    }
+
+    public double subtract (double number1, double number2){
+        return number1 - number2;
+    }
+
 }
